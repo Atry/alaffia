5b02bebd9c26311e951b49906698c0c523ea780d
arthurava <arthurcheminbayerlein@gmail.com>
2024-05-07 23:45:05 -0700

    Expand migration with function needed for usersByLocation query
    Also add to seeding so we can test 1:n relationship from location to facilities

--- migrations/current.sql
+++ migrations/current.sql
@@ -61,7 +61,8 @@ ON CONFLICT (id) DO NOTHING;;
 -- Migration to seed the locations table
 INSERT INTO public.locations (id, facility_id, state, zip, address, created_at) VALUES
 ('a8810f61-2f09-4bb8-8b7c-01b1734ddac2', 'f5af7463-983c-4a1d-8488-44c8188b8ce5', 'CA', '90001', '1234 Main St, Los Angeles', now()),
-('a21dd2ea-875e-4384-8a15-41efdb5b47b6', '40a7bdd0-e1c8-4f6f-9810-03784664ccd9', 'CA', '90005', '5678 Second St, Los Angeles', now())
+('a21dd2ea-875e-4384-8a15-41efdb5b47b6', '40a7bdd0-e1c8-4f6f-9810-03784664ccd9', 'CA', '90005', '5678 Second St, Los Angeles', now()),
+('a21dd2ea-875e-4384-8a15-41efdb5b47b7', '40a7bdd0-e1c8-4f6f-9810-03784664ccd9', 'CA', '90005', '91011 Third St, Los Angeles', now())
 ON CONFLICT (id) DO NOTHING;;
 
 -- Migration to create many-to-many relationships between users and facilities
@@ -70,3 +71,29 @@ INSERT INTO public.user_facilities (user_id, facility_id) VALUES
 ('a3fbc5cf-8c87-442c-9bb9-cfaf4926fe02', '40a7bdd0-e1c8-4f6f-9810-03784664ccd9'),
 ('d6fbc5cf-8c87-442c-9bb9-cfaf4926fe01', '40a7bdd0-e1c8-4f6f-9810-03784664ccd9')  -- John Doe is busy dude, he works for both hospitals!
 ON CONFLICT (user_id, facility_id) DO NOTHING;
+
+-- Complex functions and indices to help natively generate performant GraphQL queries on PostGraphile
+
+CREATE INDEX IF NOT EXISTS idx_user_facilities_on_user_id_facility_id ON public.user_facilities (user_id, facility_id);
+CREATE INDEX IF NOT EXISTS idx_locations_on_state ON public.locations (state);
+CREATE INDEX IF NOT EXISTS idx_locations_on_zip ON public.locations (zip);
+
+CREATE OR REPLACE FUNCTION public.filter_locations(
+    state_filter text,
+    zip_filter text,
+    address_filter text
+)
+RETURNS SETOF locations AS $$
+BEGIN
+    RETURN QUERY
+    SELECT *
+    FROM locations
+    WHERE
+        (state_filter IS NULL OR state = state_filter) AND
+        (zip_filter IS NULL OR zip = zip_filter) AND
+        (address_filter IS NULL OR address ILIKE '%' || address_filter || '%');
+END;
+$$ LANGUAGE plpgsql STABLE;
+
+COMMENT ON FUNCTION public.filter_locations(text, text, text) IS
+'Fetches locations based on optional state, zip, and address filters.';
