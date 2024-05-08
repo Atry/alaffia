cea8a8a0132b2df98f8c0a7758ce84198f4c475c
arthurava <arthurcheminbayerlein@gmail.com>
2024-05-07 18:07:07 -0700

    Setup application code for running postgraphile

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello world again');
+import express from 'express';
+import { postgraphile } from 'postgraphile';
+
+const app = express();
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:password@localhost/alaffia_db';
+const SCHEMA = 'public';
+
+app.use(
+    postgraphile(
+        DATABASE_URL,
+        SCHEMA,
+        {
+            watchPg: true,
+            graphiql: true,
+            enhanceGraphiql: true,
+        }
+    )
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () : void => {
+    console.log(`Server is running on http://localhost:${PORT}/graphiql`);
+});
